8a4a75aceb6df3f42dd0a64b535b9a6a7ff4ffd2
ShazilK47 <shazil.akn@gmail.com>
2024-11-21 19:52:10 +0500

    SparkHub

---
+++ app/(root)/page.tsx
@@ -0,0 +1,49 @@
+import StartupCard from "@/components/StartupCard";
+import SearchForm from "../../components/SearchForm";
+import { STARTUP_QUERY } from "@/sanity/lib/queries";
+import { client } from "@/sanity/lib/client";
+import { StartupTypeCard } from "@/components/StartupCard";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
+  const post = await client.fetch(STARTUP_QUERY);
+
+  console.log(JSON.stringify(post, null, 2));
+
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch your startup, <br /> connect with Entrepreneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, Get Noticed in Virtual Competitions
+        </p>
+
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search Results for ${query}` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {post?.length > 0 ? (
+            post.map((post: StartupTypeCard, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No Startups Found</p>
+          )}
+        </ul>
+      </section>
+    </>
+  );
+}
